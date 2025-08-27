352efe661c0c9be900d4ae6b645884d67899359c
eugenishef <eugenishef@yandex.ru>
2025-08-27 23:52:04 +0300

    first commit

---
+++ src/main/java/dev/sheff/license/controller/LicensingController.java
@@ -0,0 +1,9 @@
+package dev.sheff.license.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "v1/organization/{organizationId}/license")
+public class LicensingController {
+}
