955c1b6d99876545db802e445979ad2af232748d
eugenishef <eugenishef@yandex.ru>
2025-08-28 18:17:07 +0300

    Рефакторинг

--- src/main/java/dev/sheff/license/controller/LicensingController.java
+++
@@ -1,9 +0,0 @@
-package dev.sheff.license.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping(value = "v1/organization/{organizationId}/license")
-public class LicensingController {
-}
--- src/test/java/com/optimagrowth/license/LicenseServiceApplicationTests.java
+++ src/test/java/com/optimagrowth/license/LicenseServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package dev.sheff.license;
+package com.optimagrowth.license;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LicensingServiceApplicationTests {
+class LicenseServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
